76b59fdde9a9b578330b89c462b3c7476a559d8b
FixGlitch <blancolucas112@gmail.com>
2024-06-22 17:02:33 -0300

    fix app

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "your_secret_key",
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false },
